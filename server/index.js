20811f162208ac9f3272febc1c602a09b5132614
suraj bharatbhushan shinde <127151331+Suraj11000@users.noreply.github.com>
2023-12-19 12:12:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://savecertificate-front.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
